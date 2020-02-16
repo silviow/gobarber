d0cb2daf1f24bc10f50f4b43a6b9223864de0c9a
Silvio Marques <silvio@impyco.com.br>
2020-02-16 13:14:03 -0300

    Implementation of jwt user authentication

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -14,6 +14,10 @@ class UserController {
 
         return res.json({ id, name, email, provider });
     }
+
+    async update(req, res) {
+        return res.json({ ok: true });
+    }
 }
 
 export default new UserController();
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '66be268b7f30cedd0519f2da30c5958f',
+    expiresIn: '7d',
+};
