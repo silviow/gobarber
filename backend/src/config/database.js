6a524ef02f87a5149988fb0f7e548f2d2ea8fb87
Silvio Marques <silvio@impyco.com.br>
2020-02-16 10:29:47 -0300

    Creating directory structure, implementing and configuring ESLint, Prettier and Sequelize

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'gostackdb',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
