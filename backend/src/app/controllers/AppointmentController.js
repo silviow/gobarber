0e343c7970090c16ee392562f96b7976e05e6e15
Silvio Marques <silvio@impyco.com.br>
2020-02-24 19:39:18 -0300

    Including virtual fields in the appointment model

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
             order: ['date'],
             limit: 20,
             offset: (page - 1) * 20,
-            attributes: ['id', 'date'],
+            attributes: ['id', 'date', 'past', 'cancelable'],
             include: [
                 {
                     model: User,
