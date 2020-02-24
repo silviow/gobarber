d36a212f9b8cbdef47af150276da2cfd9b75fc85
Silvio Marques <silvio@impyco.com.br>
2020-02-24 14:22:47 -0300

    Adding email functionality

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -4,6 +4,7 @@ import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
     async index(req, res) {
@@ -120,7 +121,20 @@ class AppointmentController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email'],
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                },
+            ],
+        });
 
         if (appointment.user_id !== req.userId) {
             return res.status(401).json({
@@ -139,6 +153,17 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'A scheduled service has been canceled',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(appointment.date, "MMMM dd 'at' H:mm"),
+            },
+        });
 
         return res.json(appointment);
     }
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '1954bd05947780',
+        pass: '0fe5a0ad7e17f7',
+    },
+    default: {
+        from: 'GoBarber <no-reply@gobarber.com>',
+    },
+};
