7883563cfd278fdad9193d05c0acc6320e1a018a
Silvio Marques <silvio@impyco.com.br>
2020-02-23 21:40:19 -0300

    Implementation of services schedule functionality

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
-import databaseConfig from '../config/database';
+import Appointment from '../app/models/Appointment';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
     constructor() {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,6 +20,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
